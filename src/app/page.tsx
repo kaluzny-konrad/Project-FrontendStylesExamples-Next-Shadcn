0ca564827141895e62718be424ddb861e494afe0
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-04-09 19:29:00 +0200

    change styles on page start

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-20 gap-20">
-      <Link href={'/ceneo'}>Ceneo</Link>
+    <main className="flex flex-col mx-auto gap-4">
+      <p className="text-lg font-bold">Clones:</p>
+      <Link href={"/ceneo"}>Ceneo Clone</Link>
     </main>
   );
 }
