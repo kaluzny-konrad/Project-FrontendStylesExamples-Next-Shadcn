bde8d656a636f47148be64e6eddc1e928b975165
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-04-09 19:28:14 +0200

    delete wrong links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
-import ButtonTailwind from "@/components/ButtonTailwind";
-import ButtonSass from "@/components/ButtonSass";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col p-20 gap-20">
-      <ButtonSass />
-      <ButtonTailwind />
       <Link href={'/ceneo'}>Ceneo</Link>
     </main>
   );
