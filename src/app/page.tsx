a201e9fad9a54d5a73016a4a0c69bbf57207fa85
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-04-10 18:45:03 +0200

    fix mainpage styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mx-auto gap-4">
-      <p className="text-lg font-bold">Clones:</p>
-      <Link href={"/ceneo"}>Ceneo Clone</Link>
+    <main className="flex flex-col mx-auto h-screen">
+      <div className="mx-auto my-auto gap-10 flex flex-col text-center">
+        <p className="text-lg font-bold">Pages:</p>
+        <Link
+          href={"/ceneo"}
+          className={cn(
+            buttonVariants({
+              className:
+                "bg-ceneo-primary hover:bg-ceneo-secondary text-lg uppercase rounded-md font-light transition-all",
+              size: "lg",
+            })
+          )}
+        >
+          Ceneo Clone
+        </Link>
+      </div>
     </main>
   );
 }
